247260bc227656b2b30210d0d8a0bbfacd293eb9
Gustavo Geller <gustavopgeller@gmail.com>
2025-10-04 00:14:11 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,75 +1,17 @@
-import java.util.Scanner;
-
 public class Main
 {
-    public static void main (String[] args)
+    public static void main(String[] args)
     {
-        Scanner tec = new Scanner(System.in);
+        Produto p1 = new Produto("Notebook", 3499.00, 10, 2);
 
-        int op = 0;
-        String nome;
-        double valor;
-        int qtd;
+        p1.mostrarDados();
 
-        Produto produto = new Produto();
+        p1.vender(9, 10);
 
-        produto.nome = "Compiuter gueimer ultima geração 3000";
-        produto.valor = 3949.99;
-        produto.qtd = 2;
+        p1.reporQuantidade(3);
 
-        do
-        {
-            System.out.println("MENU");
-            System.out.println("1 - Escrever dados");
-            System.out.println("2 - Atribuir dados");
-            System.out.println("3 - Calcular valor total");
-            System.out.println("4 - Alterar quantidade");
-            System.out.println("5 - Sair");
+        p1.setPreco(4000.0);
 
-            op = tec.nextInt();
-
-            if (op == 1)
-            {
-                System.out.println("O nome do produto é: "+produto.nome);
-                System.out.println("O valor do produto é: R$"+produto.valor);
-                System.out.println("A quantidade em estoque é: "+produto.qtd);
-            }
-
-            else if (op == 2)
-            {
-                System.out.println("Digite o nome do produto: ");
-                
-                nome = tec.nextLine();
-                tec.next();
-                System.out.println("Digite o valor (em R$): ");
-                valor = tec.nextDouble();
-                System.out.println("Digite a quantidade: ");
-                qtd = tec.nextInt();
-
-                produto.atribuirDados(nome, valor, qtd);
-            }
-
-            else if (op == 3)
-            {
-                System.out.println("O valor total é: R$"+produto.calcularValorTotal());
-            }
-
-            else if (op == 4)
-            {
-                System.out.println("Escreva a nova quantidade: ");
-                int aux = tec.nextInt();
-                produto.alterarQuantidade(aux);
-            }
-
-            else if (op == 5)
-            {
-                break;
-            }
-
-            else
-            {
-                System.out.println("Operação inválida!");
-            }
-        } while (op != 5);
+        p1.mostrarDados();
     }
 }
--- Produto.java
+++ Produto.java
@@ -1,28 +1,95 @@
+import java.util.Random;
+
 public class Produto
 {
-    String nome;
-    Double valor;
-    int qtd;
+    private String nome;
+    private double preco;
+    private int codigo;
+    private int qtdEstoque;
+    private int estoqueMin;
 
-    public void atribuirDados(String nNome, double nValor, int nQtd)
+    public Produto(String nome, double preco, int qtdEstoque, int estoqueMin)
     {
-        this.nome = nNome;
-        this.valor = nValor;
-        this.qtd = nQtd;
+        this.nome = nome;
+        this.codigo = gerarCodigo();
+        this.qtdEstoque = qtdEstoque;
+        this.estoqueMin = estoqueMin;
+
+        if(preco < 0)
+        {
+            this.preco = 0;
+            System.out.println("O preço está inválido, favor alterar!");
+        }
+        else
+        {
+            this.preco = preco;
+        }
     }
-    // apenas teste
 
-    public double calcularValorTotal()
+    private int gerarCodigo()
     {
-        double vTotal = 0;
-        vTotal = this.valor * this.qtd;
-        return vTotal;
+        Random r = new Random();
+        int cod = r.nextInt(100, 999);
+
+        return cod;
     }
 
-    public void alterarQuantidade(int nQtd)
+    public void vender(int quantidade, int porcentoDesconto)
     {
-        this.qtd = nQtd;
+        if(quantidade > qtdEstoque)
+        {
+            System.out.println("Não foi possível vender!");
+        }
+        else
+        {
+            System.out.println("O produto foi venido com o preço de R$ "+(quantidade * (preco - aplicarDesconto(porcentoDesconto))));
 
-        System.out.println("A nova quantidade é: "+this.qtd);
+            qtdEstoque -= quantidade;
+            if(verificaEstoqueBaixo())
+            {
+                System.out.println("O estoque está baixo!");
+            }
+            
+            System.out.println("Estoque pós venda: "+qtdEstoque);
+        }
+    }
+
+    public void reporQuantidade(int quantidade)
+    {
+        if(quantidade < 0)
+        {
+            System.out.println("Não foi possível repor!");
+        }
+        else
+        {
+            System.out.println("A quantidade foi adicionada ao estoque!");
+            qtdEstoque += quantidade;
+            System.out.println("Estoque atualizado: "+qtdEstoque);
+        }
+    }
+    
+    private double aplicarDesconto(double porcento)
+    {
+        double desconto = preco * (porcento/100);
+        return desconto;
+    }
+
+    private boolean verificaEstoqueBaixo()
+    {
+        if(qtdEstoque < estoqueMin)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void mostrarDados()
+    {
+        System.out.println("Nome: "+this.nome+"\nPreço: R$"+this.preco+"\nCódigo: "+this.codigo+"\nQuantidade de estoque disponível: "+this.qtdEstoque);
+    }
+
+    public void setPreco(double preco)
+    {
+        this.preco = preco;
     }
 }
